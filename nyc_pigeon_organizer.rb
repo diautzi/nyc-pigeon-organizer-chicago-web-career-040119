69083a95e9c6633463f2f9b9c9fccd9d240e127f
Diana Utan <dia_utzi@yahoo.com>
2019-03-18 18:29:46 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -19,23 +19,3 @@ end
 pigeon_list
 end
 
-# def nyc_pigeon_organizer(data)
-#   pigeon_list = {}
-#   data.each do |key, details|                           # :color => {:purple => ["Theo", "Peter Jr.", "Lucky"]} etc
-#     details.each do |attribute, array_of_names|         # :purple => ["Theo", "Peter Jr.", "Lucky"] etc
-#       array_of_names.each do |name|                     # ["Theo", "Peter Jr.", "Lucky", "Ms. K", "Queenie", "Andrew", "Alex"]
-#         if !pigeon_list.has_key?(name)
-#           pigeon_list[name] = {}                       # "Theo" => {} etc
-#         end
-
-#         if !pigeon_list[name].has_key?(key)
-#           pigeon_list[name][key] = []                  # "Theo" => {:color => []} etc
-#         end
-#         if !pigeon_list[name][key].include?(attribute)
-#           pigeon_list[name][key] << attribute.to_s       # "Theo" => {:color => ["purple", "gray"]} etc
-#         end
-#       end
-#     end
-#   end
-#   pigeon_list
-# end
