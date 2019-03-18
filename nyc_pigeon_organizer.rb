4e09e909d971bddc1807623dafd043a0ed4385e0
Diana Utan <dia_utzi@yahoo.com>
2019-03-18 18:29:19 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,7 +7,7 @@ def nyc_pigeon_organizer(data)
   if !pigeon_list.has_key?(name)
     pigeon_list[name] = {}
   end
-  if !pigeon_name[name].has_key?(key)
+  if !pigeon_list[name].has_key?(key)
     pigeon_list[name][key] = []
   end
     if !pigeon_list[name][key].include?(details)
